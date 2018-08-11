9c37d07c304385f5d996f8f74a85c9e9f6e53cb8
Zack <Zack@DESKTOP-T4SQJO2>
2018-08-11 00:11:23 -0500

    SOLAR PANELS

--- src/com/hyprgloo/ld42/SpaceStation.java
+++ src/com/hyprgloo/ld42/SpaceStation.java
@@ -16,10 +16,12 @@ public class SpaceStation {
 		if(Game.selected_level == 1) {
 			stationParts.add(new SpaceStationPart(0f, 0f, 0f, Main.INDEX_SOLAR_PANEL));
 			stationParts.add(new SpaceStationPart(1f, 0f, 0f, Main.INDEX_SOLAR_PANEL));
+			stationParts.add(new SpaceStationPart(0f, 1f, 0f, Main.INDEX_SOLAR_PANEL));
+			stationParts.add(new SpaceStationPart(1f, 1f, 0f, Main.INDEX_SOLAR_PANEL));
 		//}else if(level == 2) {
-		//	stationParts.add(new SpaceStationPart(xArg, yArg, rotationArg, textureIndexArg));
+		//	stationParts.add(new SpaceStationPart(xGridArg, yGridArg, rotationArg, textureIndexArg));
 		//}else if(level == 3) {
-		//	stationParts.add(new SpaceStationPart(xArg, yArg, rotationArg, textureIndexArg));
+		//	stationParts.add(new SpaceStationPart(xGridArg, yGridArg, rotationArg, textureIndexArg));
 
 		}
 	}
