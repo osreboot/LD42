b0fdb7b632d10f12bdd1cc2a4c430c88599ef87b
Zack <Zack@DESKTOP-T4SQJO2>
2018-08-11 00:46:20 -0500

    erdfgg

--- src/com/hyprgloo/ld42/SpaceStation.java
+++ src/com/hyprgloo/ld42/SpaceStation.java
@@ -13,15 +13,17 @@ public class SpaceStation {
 	public static ArrayList<SpaceStationPart> stationParts = new ArrayList<>();
 
 	public static void restart(){
+		//0,0 is the center of the screen
 		if(Game.selected_level == 1) {
 			stationParts.add(new SpaceStationPart(0f, 0f, 0f, Main.INDEX_SOLAR_PANEL));
 			stationParts.add(new SpaceStationPart(1f, 0f, 0f, Main.INDEX_SOLAR_PANEL));
 			stationParts.add(new SpaceStationPart(0f, 1f, 0f, Main.INDEX_SOLAR_PANEL));
 			stationParts.add(new SpaceStationPart(1f, 1f, 0f, Main.INDEX_SOLAR_PANEL));
-		//}else if(level == 2) {
-		//	stationParts.add(new SpaceStationPart(xGridArg, yGridArg, rotationArg, textureIndexArg));
-		//}else if(level == 3) {
-		//	stationParts.add(new SpaceStationPart(xGridArg, yGridArg, rotationArg, textureIndexArg));
+		}else if(Game.selected_level == 2) {
+			stationParts.add(new SpaceStationPart(0f, 0f, 0f, Main.INDEX_SOLAR_PANEL));
+			stationParts.add(new SpaceStationPart(1f, 1f, 0f, Main.INDEX_SOLAR_PANEL));
+		}else if(Game.selected_level == 3) {
+			stationParts.add(new SpaceStationPart(1f, 0f, 0f, Main.INDEX_SOLAR_PANEL));
 
 		}
 	}
