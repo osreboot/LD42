f0bc8946e31f0bda34d3e20524d6f02d6d565e15
Zack <Zack@DESKTOP-T4SQJO2>
2018-08-11 00:06:53 -0500

    bn bvn

--- src/com/hyprgloo/ld42/SpaceStationPart.java
+++ src/com/hyprgloo/ld42/SpaceStationPart.java
@@ -11,8 +11,8 @@ public class SpaceStationPart {
 	public int textureIndex;
 	
 	public SpaceStationPart(float xArg, float yArg, float rotationArg, int textureIndexArg){
-		x = Display.getWidth()/2; //+ (GRID_SIZE/2 * xArg);
-		y = Display.getHeight()/2; //+ (GRID_SIZE/2 * yArg);
+		x = Display.getWidth()/2 + (GRID_SIZE * xArg);
+		y = Display.getHeight()/2 + (GRID_SIZE * yArg);
 		
 		rotation = rotationArg;
 		textureIndex = textureIndexArg;
