507a2567fcdb4ee4e94190e3c7398e0cc73c12cb
Zack <Zack@DESKTOP-T4SQJO2>
2018-08-11 00:49:14 -0500

    rotation

--- src/com/hyprgloo/ld42/SpaceStation.java
+++ src/com/hyprgloo/ld42/SpaceStation.java
@@ -1,6 +1,8 @@
 package com.hyprgloo.ld42;
 
 import static com.osreboot.ridhvl.painter.painter2d.HvlPainter2D.hvlDrawQuadc;
+import static com.osreboot.ridhvl.painter.painter2d.HvlPainter2D.hvlResetRotation;
+import static com.osreboot.ridhvl.painter.painter2d.HvlPainter2D.hvlRotate;
 
 import java.util.ArrayList;
 
@@ -16,7 +18,7 @@ public class SpaceStation {
 		//0,0 is the center of the screen
 		if(Game.selected_level == 1) {
 			stationParts.add(new SpaceStationPart(0f, 0f, 0f, Main.INDEX_SOLAR_PANEL));
-			stationParts.add(new SpaceStationPart(1f, 0f, 0f, Main.INDEX_SOLAR_PANEL));
+			stationParts.add(new SpaceStationPart(1f, 0f, 90f, Main.INDEX_SOLAR_PANEL));
 			stationParts.add(new SpaceStationPart(0f, 1f, 0f, Main.INDEX_SOLAR_PANEL));
 			stationParts.add(new SpaceStationPart(1f, 1f, 0f, Main.INDEX_SOLAR_PANEL));
 		}else if(Game.selected_level == 2) {
@@ -29,14 +31,16 @@ public class SpaceStation {
 	}
 
 
-	
+
 	public static void update(float delta){
-		
-			for(SpaceStationPart part : stationParts) {
+
+		for(SpaceStationPart part : stationParts) {
+			hvlRotate(part.x, part.y, part.rotation);
 			hvlDrawQuadc(part.x, part.y, GRID_SIZE, GRID_SIZE, Main.getTexture(part.textureIndex));
-			}
-		
-		
+			hvlResetRotation();
+		}
+
+
 	}
 
 }
