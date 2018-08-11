059ae393c95a40b08366d29a4d1962b19b17468f
Zack <Zack@DESKTOP-T4SQJO2>
2018-08-10 22:29:18 -0500

    AAAAAAAAAAHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH

--- src/com/hyprgloo/ld42/SpaceStation.java
+++ src/com/hyprgloo/ld42/SpaceStation.java
@@ -1,8 +1,23 @@
 package com.hyprgloo.ld42;
 
+import java.util.ArrayList;
+
 public class SpaceStation {
 
 	public static final float GRID_SIZE = 24f;
+	
+	public static ArrayList<SpaceStation> stationGrid = new ArrayList<>();
+	
+	public float x, y, rotation;
+	public int textureIndex;
+	
+	public SpaceStation(float xArg, float yArg, float rotationArg, int textureIndexArg){
+		x = xArg;
+		y = yArg;
+		rotation = rotationArg;
+		textureIndex = textureIndexArg;
+		stationGrid.add(this);
+	}
 	
 	public static void restart(){
 		
