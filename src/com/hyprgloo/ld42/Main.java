90bc22660c150b2dd6f53ab612dda205b5a0f3ee
haveaniceday33 <munrosamuel@gmail.com>
2018-08-12 17:49:15 -0400

    Cursor defaults when paused

--- src/com/hyprgloo/ld42/Main.java
+++ src/com/hyprgloo/ld42/Main.java
@@ -75,7 +75,8 @@ public class Main extends HvlTemplateInteg2D{
 	INDEX_LIGHT_1 = 25,
 	INDEX_LIGHT_2 = 26,
 	INDEX_LIGHT_3 = 27,
-	INDEX_CURSOR_2 = 28;
+	INDEX_CURSOR_2 = 28,
+	INDEX_CLEAR = 29;
 
 	public static final Color 
 	COLOR_GREEN0 = new Color(0f, 1.0f, 0f),
@@ -128,6 +129,7 @@ public class Main extends HvlTemplateInteg2D{
 		getTextureLoader().loadResource("lightning2"); //26
 		getTextureLoader().loadResource("lightning3"); //27
 		getTextureLoader().loadResource("cursor2"); //28
+		getTextureLoader().loadResource("clear"); //29
 
 
 		font = new HvlFontPainter2D(getTexture(INDEX_FONT), HvlFontPainter2D.Preset.FP_AGOFFICIAL);
@@ -170,8 +172,16 @@ public class Main extends HvlTemplateInteg2D{
 	@Override
 	public void update(float delta){
 		if(HvlMenu.getCurrent() == MenuManager.pause) {
+			HvlCursor.setTexture(getTexture(INDEX_CLEAR));
 			HvlCursor.setNativeHidden(false);
 		} else {
+			if(HvlCursor.getTexture() == getTexture(INDEX_CLEAR)) {
+				HvlCursor.setTexture(Main.getTexture(Main.INDEX_CURSOR_2));
+				HvlCursor.setHeight(32);
+				HvlCursor.setWidth(32);
+				HvlCursor.setXOffset(-HvlCursor.getWidth()/2);
+				HvlCursor.setYOffset(-HvlCursor.getHeight()/2);
+			}
 			HvlCursor.setNativeHidden(true);
 		}
 		MenuManager.update(delta);
